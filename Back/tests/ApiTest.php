040cfeebaa8870cd4b8fa0a92b0c598c0388c7d1
Antony CASTANER <antony.castaner@ynov.com>
2022-03-04 10:37:30 +0100

    Add api tests

--- Back/tests/ApiTest.php
+++ Back/tests/ApiTest.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ApiTest extends WebTestCase
 {
-    public function testApiAddition(): void
+    public function testHome(): void
     {
         $client = static::createClient();
         // Request a specific page
@@ -16,5 +16,91 @@ class ApiTest extends WebTestCase
         $this->assertJson($response->getContent());
         $responseData = json_decode($response->getContent(), true);
         $this->assertEquals(['message' => "Hello world"], $responseData);
+    }
+
+    public function testProductsIndex(): void
+    {
+        $client = static::createClient();
+        // Request a specific page
+        $client->jsonRequest('GET', '/api/products');
+        $response = $client->getResponse();
+        $this->assertResponseIsSuccessful();
+        $this->assertJson($response->getContent());
+        $responseData = json_decode($response->getContent(), true);
+        $this->assertIsArray($responseData);
+    }
+
+    public function testProductShow(): void
+    {
+        $client = static::createClient();
+        // Request a specific page
+        $client->jsonRequest('GET', '/api/products/1');
+        $response = $client->getResponse();
+        $this->assertResponseIsSuccessful();
+        $this->assertJson($response->getContent());
+        $responseData = json_decode($response->getContent(), true);
+        $this->assertArrayHasKey('id', $responseData);
+        $this->assertArrayHasKey('image', $responseData);
+        $this->assertArrayHasKey('name', $responseData);
+    }
+
+    public function testCartStoreProduct(): void
+    {
+        $client = static::createClient();
+        // Request a specific page
+        $client->jsonRequest('POST', '/api/cart/1', array(
+            'quantity' => 2
+        ));
+        $response = $client->getResponse();
+        $this->assertResponseIsSuccessful();
+        $this->assertJson($response->getContent());
+        $responseData = json_decode($response->getContent(), true);
+        $this->assertArrayHasKey('id', $responseData);
+        $this->assertArrayHasKey('products', $responseData);
+    }
+
+    public function testCartStoreProductWithException(): void
+    {
+        $client = static::createClient();
+        // Request a specific page
+        $client->jsonRequest('POST', '/api/cart/1', array(
+            'quantity' => 100
+        ));
+        $response = $client->getResponse();
+        $this->assertResponseIsSuccessful();
+        $this->assertJson($response->getContent());
+        $responseData = json_decode($response->getContent(), true);
+        $this->assertEquals(['error' => 'too many'], $responseData);
+    }
+
+    public function testCartShow(): void
+    {
+        $client = static::createClient();
+        // Request a specific page
+        $client->jsonRequest('GET', '/api/cart');
+        $response = $client->getResponse();
+        $this->assertResponseIsSuccessful();
+        $this->assertJson($response->getContent());
+        $responseData = json_decode($response->getContent(), true);
+        $this->assertArrayHasKey('id', $responseData);
+        $this->assertArrayHasKey('products', $responseData);
+    }
+
+    public function testCartRemoveProduct(): void
+    {
+        $client = static::createClient();
+        // Request a specific page
+        $client->jsonRequest('DELETE', '/api/cart/1');
+        $response = $client->getResponse();
+        $this->assertResponseIsSuccessful();
+        $this->assertJson($response->getContent());
+        $responseData = json_decode($response->getContent(), true);
+        $this->assertNotContainsEquals(array(
+            'id' => 1,
+            'name' => 'Rick Sanchez',
+            'price' => '9,99',
+            'quantity' => 2,
+            'image' => 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+        ), $responseData);
     }
 }
