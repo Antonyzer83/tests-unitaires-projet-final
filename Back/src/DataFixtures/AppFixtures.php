2d8a893c25fe5a19de975ffaff84d99ab406adea
Antony CASTANER <antony.castaner@ynov.com>
2022-03-04 17:09:52 +0100

    Try ci with docker for back

--- Back/src/DataFixtures/AppFixtures.php
+++ Back/src/DataFixtures/AppFixtures.php
@@ -13,8 +13,8 @@ use App\Service\RickAndMortyApiService;
 class AppFixtures extends Fixture
 {
     private RickAndMortyApiService $rickAndMortyService;
-    private $prices = ["8", "9,99", "15", "16.50", "20"];
-    private $quantites = [0, 2, 5, 20, 30, 70];
+    private $prices = "9,99";
+    private $quantites = 2;
 
     public function __construct(RickAndMortyApiService $rickAndMortyService)
     {
@@ -27,8 +27,8 @@ class AppFixtures extends Fixture
         foreach ($data as $model) {
             $product = new Product();
             $product->setName($model->getName());
-            $product->setPrice($this->prices[array_rand($this->prices, 1)]);
-            $product->setQuantity($this->quantites[array_rand($this->quantites, 1)]);
+            $product->setPrice($this->prices);
+            $product->setQuantity($this->quantites);
             $product->setImage($model->getImage());
             $manager->persist($product);
         }
