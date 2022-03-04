4d4f8493eafc0b7829ad04ae885c03f8600b2f40
Antony CASTANER <antony.castaner@ynov.com>
2022-03-04 12:07:28 +0100

    Fix useCart with API mock

--- Front/src/tests/hooks/useCart.test.tsx
+++ Front/src/tests/hooks/useCart.test.tsx
@@ -1,6 +1,8 @@
 import { renderHook } from "@testing-library/react-hooks";
 import { act } from "react-dom/test-utils";
 import useCart from '../../hooks/useCart';
+import { setupServer } from "msw/node";
+import { rest } from "msw";
 
 const newProduct = {
   id: 1,
@@ -10,23 +12,61 @@ const newProduct = {
   image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
 };
 
-test('loadCart', () => {
+const server = setupServer(
+  rest.get(
+    "http://localhost:8000/api/cart",
+    (req, res, ctx) => {
+      return res(
+        ctx.json({
+          id: 1,
+          products: []
+        })
+      );
+    }
+  ),
+  rest.delete(
+    "http://localhost:8000/api/cart/1",
+    (req, res, ctx) => {
+      return res(
+        ctx.json({
+          id: 1,
+          products: []
+        })
+      );
+    }
+  )
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+test('loadCart', async () => {
   const { result } = renderHook(() => useCart());
-  const { loadCart, products, loading } = result.current;
-  act(() => {
-    loadCart();
+  const { loadCart, loading } = result.current;
+
+  expect(loading).toEqual(true);
+
+  await act(async () => {
+    await loadCart();
   });
   
-  expect(products).not.toEqual([]);
-  expect(loading).toEqual(false);
+  const { products } = result.current;
+  expect(products).toEqual(
+    expect.arrayContaining([])
+  );
 });
 
-test('removeToCart', () => {
+test('removeToCart', async () => {
   const { result } = renderHook(() => useCart());
-  const { removeToCart, message } = result.current;
-  act(() => {
-    removeToCart(newProduct);
+  const { removeToCart, loading } = result.current;
+
+  expect(loading).toEqual(true);
+
+  await act(async () => {
+    await removeToCart(newProduct);
   });
 
+  const { message } = result.current;
   expect(message).toEqual('Produit bien supprimé');
 });
